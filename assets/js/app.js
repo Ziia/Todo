cd30ba4e5b5bfcdf4093bfbc7fbfa83a4d83419e
Ziia <Ziarobin@gmail.com>
2017-01-11 15:45:04 +0100

    Add files via upload

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("done");
+});
+
+$("ul").on("click", "span", function(event) {
+  //  stopPropagation Stops other events to be triggered. "event bubbling"
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+  if(event.which === 13) {
+    var todoText = $(this).val();
+    $("ul").append("<li><span><i class='fa fa-trash-o'></i> </span> " + todoText + "</li>");
+    $(this).val("");
+  }
+});
+
+$(".fa-plus").on("click", function() {
+  $("input[type='text']").fadeToggle();
+});
